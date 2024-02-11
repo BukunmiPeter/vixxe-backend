a8057912faf37e3ecdfe5bc9d1678f94bf22d266
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2024-02-11 18:12:41 +0100

    ...

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,22 @@
   import {Express, Request, Response} from "express";
+import { createUserSchema } from "./schema/user.schema";
 
+import validateResource from "./middleware/validateResource";
+
+import { createUserHandler } from "./controllers/user.controller";
+import { createUserSessionHandler } from "./controllers/session.controller.";
+import { createSessionSchema } from "./schema/session.schema";
   function routes(app: Express) {
 
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+    app.post("/api/users", validateResource(createUserSchema), createUserHandler);
+
+     app.post(
+    "/api/sessions",
+    validateResource(createSessionSchema),
+    createUserSessionHandler
+  );
   }
 
   export default routes
