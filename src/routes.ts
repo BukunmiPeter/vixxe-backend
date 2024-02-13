a2905123c8b6ffaf73b6c13a157a53445eae5cf7
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2024-02-13 18:44:03 +0100

    products

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,7 @@ import { createUserHandler } from "./controllers/user.controller";
 import { createUserSessionHandler, deleteSessionHandler, getUserSessionsHandler } from "./controllers/session.controller.";
 import { createSessionSchema } from "./schema/session.schema";
 import requireUser from "./middleware/requireUser";
+import { createProductSchema, deleteProductSchema, getProductSchema, updateProductSchema } from "./schema/product.schema";
   function routes(app: Express) {
 
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
@@ -21,6 +22,30 @@ import requireUser from "./middleware/requireUser";
    app.get("/api/sessions", requireUser, getUserSessionsHandler);
 
      app.delete("/api/sessions", requireUser, deleteSessionHandler);
+
+       app.post(
+    "/api/products",
+    [requireUser, validateResource(createProductSchema)],
+    createProductHandler
+  );
+
+  app.put(
+    "/api/products/:productId",
+    [requireUser, validateResource(updateProductSchema)],
+    updateProductHandler
+  );
+
+  app.get(
+    "/api/products/:productId",
+    validateResource(getProductSchema),
+    getProductHandler
+  );
+
+  app.delete(
+    "/api/products/:productId",
+    [requireUser, validateResource(deleteProductSchema)],
+    getProductHandler
+  );
   }
 
   export default routes
--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -33,7 +33,7 @@ export async function reIssueAccessToken({
   if (!decoded || !get(decoded, "session")) return false;
 
   const session = await SessionModel.findById(get(decoded, "session"));
-
+ 
   if (!session || !session.valid) return false;
 
   const user = await findUser({ _id: session.user });
